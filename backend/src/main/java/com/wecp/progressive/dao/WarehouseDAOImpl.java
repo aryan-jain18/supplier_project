d29ee40160b529dcdc97900e9cd0a55e9600915b
username <wecp-dummy@gmail.com>
2024-12-28 13:09:04 +0000

    wecp initial commit

---
+++ backend/src/main/java/com/wecp/progressive/dao/WarehouseDAOImpl.java
@@ -0,0 +1,41 @@
+package com.wecp.progressive.dao;
+
+import java.util.List;
+
+import com.wecp.progressive.entity.Warehouse;
+
+public class WarehouseDAOImpl implements WarehouseDAO{
+
+    @Override
+    public int addWarehouse(Warehouse warehouse) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void deleteWarehouse(int warehouseId) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<Warehouse> getAllWarehouse() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Warehouse getWarehouseById(int warehouseId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateWarehouse(Warehouse warehouse) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
+
+}
