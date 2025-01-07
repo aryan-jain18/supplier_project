16a0b4e988e455d1fb91eb42b681c237f974048e
username <wecp-dummy@gmail.com>
2025-01-07 15:18:38 +0000

    Day 8 Pass

--- backend/src/main/java/com/wecp/progressive/SupplyLinkApplication.java
+++ backend/src/main/java/com/wecp/progressive/SupplyLinkApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SupplyLinkApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to SupplyLink Project!");
-        // SpringApplication.run(SupplyLinkApplication.class, args);
+        SpringApplication.run(SupplyLinkApplication.class, args);
     }
 }
