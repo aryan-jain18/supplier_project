1494786cc93b1d4ba462c751af12c51b20f9e270
username <wecp-dummy@gmail.com>
2025-01-31 12:31:39 +0000

    Day 20 Pass Component of Warehouse and product ts file only

--- frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
+++ frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
@@ -1,5 +1,34 @@
 
-export class WarehouseComponent {
- 
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
-}
+
+@Component({
+    selector: 'app-warehouse',
+    templateUrl: './warehouse.component.html',
+    styleUrls: ['./warehouse.component.scss']
+})
+export class WarehouseComponent implements OnInit {
+
+    warehouseForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+  
+  }
+  ngOnInit(): void {
+    this.warehouseForm = this.fb.group({
+      warehouseId:  [null, Validators.required],
+      supplierId: [null, [Validators.required, Validators.min(1)]],
+      warehouseName: ['', Validators.required],
+      location: ['', Validators.required],
+      capacity: [null, [Validators.required, Validators.min(0)]]
+
+    });
+  }
+
+   onSubmit() : void{
+    
+    }
+   }
+
