548eafcd372499fc7c5a2e25a936a655e4b14128
username <wecp-dummy@gmail.com>
2025-02-01 12:54:10 +0000

    Day 21 Pass

--- frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
+++ frontend/src/app/supplylink/components/warehouse/warehouse.component.ts
@@ -1,34 +1,44 @@
-
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Observable, of } from "rxjs";
 
-
-@Component({
-    selector: 'app-warehouse',
-    templateUrl: './warehouse.component.html',
-    styleUrls: ['./warehouse.component.scss']
+@Component ({
+    selector : 'app-warehouse',
+    template : './warehouse.component.html',
+    styleUrls : ['./warehouse.component.scss']
 })
-export class WarehouseComponent implements OnInit {
 
-    warehouseForm: FormGroup;
+export class WarehouseComponent {
 
+  warehouseForm: FormGroup;
+  warehouseError$: Observable<string>;
+  warehouseSuccess$: Observable<string>;
 
   constructor(private fb: FormBuilder) {
-  
+    this.warehouseError$ = of('');
+    this.warehouseSuccess$ = of('');
   }
+
   ngOnInit(): void {
     this.warehouseForm = this.fb.group({
-      warehouseId:  [null, Validators.required],
-      supplierId: [null, [Validators.required, Validators.min(1)]],
+      supplierId: ['', [Validators.required]],
       warehouseName: ['', Validators.required],
-      location: ['', Validators.required],
-      capacity: [null, [Validators.required, Validators.min(0)]]
-
+      location : ['', [Validators.required]],
+      capacity: [0, [Validators.required, Validators.min(0)]]
     });
   }
 
-   onSubmit() : void{
-    
+  onSubmit(): void {
+    if (this.warehouseForm.valid) {
+      const newSupplier = this.warehouseForm.value;
+      console.log('New Supplier:', newSupplier);
+      // Handle form submission, e.g., call a service to save the supplier
+      this.warehouseSuccess$ = of('Supplier added successfully!');
+      this.warehouseError$ = of('');
+    } else {
+      this.warehouseError$ = of('Form is invalid. Please fix the errors and try again.');
+      this.warehouseSuccess$ = of('');
     }
-   }
+  }
 
+}
